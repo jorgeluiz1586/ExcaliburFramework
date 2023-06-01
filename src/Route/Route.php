53031c25bb4e52cb3c685b4dcdc1d351af76f1d6
Jorge Luiz <jorgeluiz1586@gmail.com>
2023-05-31 21:28:08 -0300

    fix: fix Route mtethod return

--- src/Route/Route.php
+++ src/Route/Route.php
@@ -27,84 +27,114 @@ class Route
         return $middleware;
     }
 
-    public static function post(string $path, callable|string|array $handle): object
+    public static function post(string $path, callable|string|array $handle): Handler
     {
         if (gettype($handle) === "string") {
-            return self::getControllerAndActionFromString($path, $handle, "POST");
+           self::getControllerAndActionFromString($path, $handle, "POST");
         } elseif (is_callable($handle)) {
-            return Router::setRoute("POST", "/api".($path === "/" ? "" : $path), null, $handle);
+           Router::setRoute("POST", "/api".($path === "/" ? "" : $path), null, $handle);
         } elseif (gettype($handle === "array")) {
             if (count($handle) === 2) {
-                return Router::setRoute("POST", "/api".($path === "/" ? "" : $path), $handle[0], $handle[1]);
+               Router::setRoute("POST", "/api".($path === "/" ? "" : $path), $handle[0], $handle[1]);
             }
         } else {
             throw (new \Exception("Error! Route is invalid"));
         }
+
+        $middleware = new Handler();
+        $middleware->type = "api";
+        $middleware->path = $path;
+        return $middleware;
     }
 
-    public static function put(string $path, callable|string|array $handle): object
+    public static function put(string $path, callable|string|array $handle): Handler
     {
         if (gettype($handle) === "string") {
-            return self::getControllerAndActionFromString($path, $handle, "PUT");
+            self::getControllerAndActionFromString($path, $handle, "PUT");
         } elseif (is_callable($handle)) {
-            return Router::setRoute("PUT", "/api".($path === "/" ? "" : $path), null, $handle);
+            Router::setRoute("PUT", "/api".($path === "/" ? "" : $path), null, $handle);
         } elseif (gettype($handle === "array")) {
             if (count($handle) === 2) {
-                return Router::setRoute("PUT", "/api".($path === "/" ? "" : $path), $handle[0], $handle[1]);
+                Router::setRoute("PUT", "/api".($path === "/" ? "" : $path), $handle[0], $handle[1]);
             }
         } else {
             throw (new \Exception("Error! Route is invalid"));
         }
+
+        $middleware = new Handler();
+        $middleware->type = "api";
+        $middleware->path = $path;
+        return $middleware;
     }
 
-    public static function patch(string $path, callable|string|array $handle): object
+    public static function patch(string $path, callable|string|array $handle): Handler
     {
         if (gettype($handle) === "string") {
-            return self::getControllerAndActionFromString($path, $handle, "PATCH");
+            self::getControllerAndActionFromString($path, $handle, "PATCH");
         } elseif (is_callable($handle)) {
-            return Router::setRoute("PATCH", "/api".($path === "/" ? "" : $path), null, $handle);
+            Router::setRoute("PATCH", "/api".($path === "/" ? "" : $path), null, $handle);
         } elseif (gettype($handle === "array")) {
             if (count($handle) === 2) {
-                return Router::setRoute("PATCH", "/api".($path === "/" ? "" : $path), $handle[0], $handle[1]);
+                Router::setRoute("PATCH", "/api".($path === "/" ? "" : $path), $handle[0], $handle[1]);
             }
         } else {
             throw (new \Exception("Error! Route is invalid"));
         }
+
+        $middleware = new Handler();
+        $middleware->type = "api";
+        $middleware->path = $path;
+        return $middleware;
     }
 
-    public static function delete(string $path, callable|string|array $handle): object
+    public static function delete(string $path, callable|string|array $handle): Handler
     {
         if (gettype($handle) === "string") {
-            return self::getControllerAndActionFromString($path, $handle, "DELETE");
+            self::getControllerAndActionFromString($path, $handle, "DELETE");
         } elseif (is_callable($handle)) {
-            return Router::setRoute("DELETE", "/api".($path === "/" ? "" : $path), null, $handle);
+            Router::setRoute("DELETE", "/api".($path === "/" ? "" : $path), null, $handle);
         } elseif (gettype($handle === "array")) {
             if (count($handle) === 2) {
-                return Router::setRoute("DELETE", "/api".($path === "/" ? "" : $path), $handle[0], $handle[1]);
+                Router::setRoute("DELETE", "/api".($path === "/" ? "" : $path), $handle[0], $handle[1]);
             }
         } else {
             throw (new \Exception("Error! Route is invalid"));
         }
+
+        $middleware = new Handler();
+        $middleware->type = "api";
+        $middleware->path = $path;
+        return $middleware;
     }
 
-    public static function apiResource(string $path, callable|string|array $handle): object
+    public static function apiResource(string $path, callable|string|array $handle): Handler
     {
         if (gettype($handle) === "string") {
             $controller = "Application\\Controllers\\".explode("@", $handle)[0];
             $action = explode("@", $handle)[1];
 
-            return Router::setRoute("", "/api".($path === "/" ? "" : $path), $controller, $action);
+            Router::setRoute("", "/api".($path === "/" ? "" : $path), $controller, $action);
         }
+
+        $middleware = new Handler();
+        $middleware->type = "api";
+        $middleware->path = $path;
+        return $middleware;
     }
 
-    public static function resource(string $path, callable|string|array $handle): object
+    public static function resource(string $path, callable|string|array $handle): Handler
     {
         if (gettype($handle) === "string") {
             $controller = "Application\\Controllers\\".explode("@", $handle)[0];
             $action = explode("@", $handle)[1];
 
-            return Router::setRoute("", "/api".($path === "/" ? "" : $path), $controller, $action);
+            Router::setRoute("", "/api".($path === "/" ? "" : $path), $controller, $action);
         }
+
+        $middleware = new Handler();
+        $middleware->type = "api";
+        $middleware->path = $path;
+        return $middleware;
     }
 
     private static function getControllerAndActionFromString(string $path, string $handle, string $method): void
